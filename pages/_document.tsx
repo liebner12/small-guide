02a71abd16bfee0bf870d2c14ea1d6881c9b6839
Michal <liebner15@gmail.com>
2021-11-29 00:10:16 +0100

    fix

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -21,9 +21,23 @@ class MyDocument extends Document {
             name="description"
             content="Small guide is the app you need for your trip to be successful"
           />
+          <meta name="application-name" content="Small guide" />
+          <meta name="apple-mobile-web-app-capable" content="yes" />
+          <meta
+            name="apple-mobile-web-app-status-bar-style"
+            content="default"
+          />
+          <meta name="apple-mobile-web-app-title" content="Small guide" />
+          <meta
+            name="description"
+            content="Small guide is the app you need for your trip to be successful"
+          />
+          <meta name="format-detection" content="telephone=no" />
+          <meta name="mobile-web-app-capable" content="yes" />
           <link rel="icon" href="/images/favicon.ico" />
           <link rel="manifest" href="/manifest.json" />
-          <meta name="theme-color" content="#202124" />
+          <link rel="shortcut icon" href="/images/favicon.ico" />
+          <meta name="theme-color" content="#1A4150" />
         </Head>
         <body>
           <Main />
